634a974370d1814929e5a56b5b933e24367366f8
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-29 21:33:06 -0500

    Create gather_type.java

---
+++ gather_type.java
@@ -0,0 +1,34 @@
+package gather_type.gather_type;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import write.write.write;
+
+public class gather_type {
+  // Global variables that will be used throughout gather_type
+  public String type_chosen;
+  public String types;
+  public boolean is_ready = false;
+  public int max = 0;
+  public write write_to_file = new write();
+
+  public void setTheType(String[] types,String type_is, ArrayList<String> add_to_data) {
+    this.type_chosen = type_is;
+    this.types = Arrays.toString(types);
+
+    // This will iterate through this.types and find the index of this.type_chosen
+    for(int i = 0; i < this.types.length(); i++) {
+      if(this.type_chosen.equals(types[i])) {
+        this.type_chosen = "type: "+this.type_chosen;
+        add_to_data.add(this.type_chosen);
+        this.is_ready = true;
+        break;
+      }
+    }
+
+    do {
+      this.write_to_file.write_data(this.type_chosen,add_to_data,this.is_ready);
+      break;
+    } while(this.is_ready);
+  }
+}
