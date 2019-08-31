c1e4fe02ddbcb1834d5d82401c657746791e45e0
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-30 20:18:03 -0500

    Update write.java

--- write.java
+++ write.java
@@ -1,22 +1,28 @@
 package write.write;
 
-import java.util.ArrayList;
-import java.util.Formatter;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.*; 
+import convert_to_json.convert_to_json.convert_to_json;
 
 public class write {
 
-  public Scanner get_dir = new Scanner(System.in);
+  private String data_is;
+  private convert_to_json converter = new convert_to_json();
 
   public void write_data(ArrayList<String> data_to_write, ArrayList<String> add_more, boolean is_ready) {
-    System.out.print("(FULL)Directory of .txt file >> ");
-    String dir = get_dir.nextLine();
+    for(String d: data_to_write) {
+      this.data_is = d;
+    }
     while(is_ready) {
       try {
-        Formatter writing = new Formatter(dir);
-        writing.format("%s",data_to_write);
-        writing.close();
-      } catch (Exception e) {
+        File create_txt_file = new File("//home//runner//info.txt");
+        FileWriter write_file = new FileWriter(create_txt_file);
+        write_file.write(this.data_is);
+        write_file.close();
+        converter.JSON_IT(this.data_is);
+      } catch(Exception e) {
         System.out.println(e);
       }
       break;
