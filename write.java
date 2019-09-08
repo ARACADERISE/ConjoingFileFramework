c3795553bae57f7fbfd539bde5044bfa4b8cb6bb
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-09-08 12:13:07 -0500

    Update write.java

--- write.java
+++ write.java
@@ -1,22 +1,20 @@
 package write.write;
 
 import java.io.File;
-//import java.io.FileOutputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.util.*; 
-import convert_to_json.convert_to_json.convert_to_json;
 
 public class write {
 
   private String data_is;
   private String insert_by_data;
-  private convert_to_json converter = new convert_to_json();
 
-  public String get_data(String data_info, boolean can_read) {
+  public String get_data(String file_con_to, String data_info, boolean can_read) {
     if(can_read) {
       this.insert_by_data = data_info;
-      System.out.println("Connected To: " + this.insert_by_data);
-      return this.insert_by_data;
+      System.out.println("Connected To: " + file_con_to);
+      return this.insert_by_data + "\n" + file_con_to;
     } else {
       return "Failed";
     }
@@ -32,7 +30,6 @@ public class write {
         FileWriter write_file = new FileWriter(create_txt_file);
         write_file.write("DATA: "+this.data_is);
         write_file.close();
-        converter.JSON_IT(this.data_is);
       } catch(Exception e) {
         System.out.println(e);
       }
