ee5ae3bdf23195310300735beef206f7d76e9b02
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-09-08 12:12:46 -0500

    Update use_type.java

--- use_type.java
+++ use_type.java
@@ -1,11 +1,12 @@
 package use_type.use_type;
 
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import use_type_setup.use_type_setup.use_type_setup;
 import write.write.write;
+import convert_to_json.convert_to_json.convert_to_json;
 
 public class use_type extends use_type_setup {
   // Globabl variables
@@ -13,6 +14,9 @@ public class use_type extends use_type_setup {
   private Scanner get_line = new Scanner(System.in);
   private ArrayList<String> return_data;
   private write w_f = new write();
+  private convert_to_json json_it = new convert_to_json();
+  // This will be used within the "connect" category
+  private String s;
 
   // Overriding the function, or calls
   @Override
@@ -27,30 +31,37 @@ public class use_type extends use_type_setup {
       System.out.print("Port Name: ");
       String get_port_type = this.get_line.nextLine();
       data.add("port_name::-->"+get_port_type);
+      json_it.GET_NAME_INFO(data.get(1));
       this.return_data = data;
     } else if(this.type.equals("connect")) {
       System.out.print("File Name: ");
-      String file_dir = get_line.nextLine();
+      String file_dir = this.get_line.nextLine();
       File con_to_dir = new File(file_dir);
+      Scanner read_file = new Scanner(con_to_dir);
+      do {
+        this.s = read_file.next();
+        json_it.GET_FILE_READ_INFO(read_file.next());
+      } while(read_file.hasNext());
       if(con_to_dir.canRead()) {
         System.out.println("\n\nConnection Made\nStatus 202");
       } else {
         throw new Exception("Error whilst connecting");
       }
-      w_f.get_data(file_dir, con_to_dir.canRead());
+      w_f.get_data(file_dir,this.s, con_to_dir.canRead());
       data.add("connect_to_file::-->"+con_to_dir);
+      json_it.GET_NAME_INFO(data.get(1));
       this.return_data = data;
     } else if(this.type.equals("reg")) {
       System.out.println(this.type);
     } else {
       throw new Exception("Type could not be confirmed");
     }
+    json_it.JSON_IT(this.type);
   }
   @Override
   public String return_all_vals() {
     // Taking this.return_data from ArrayList to a simple string
     // So we can return String
-
     for(int i = 0; i < this.return_data.size()-1; i++) {
       String s = this.return_data.get(i)+", "+this.return_data.get(i+1);
       return s;
