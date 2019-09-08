3b7d6967ae4882704733a4eb292ccd47163f9644
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-09-08 12:11:28 -0500

    Update convert_to_json.java

--- convert_to_json.java
+++ convert_to_json.java
@@ -1,17 +1,27 @@
 package convert_to_json.convert_to_json;
 
-import java.io.File;
-import java.util.Formatter;
+import java.io.*;
+import java.util.*;
 
 public class convert_to_json {
 
   private final String quote = "\"";
+  private String data_info;
+  private String file_read_info;
+
+  public void GET_FILE_READ_INFO(String file_read_info) {
+    this.file_read_info = file_read_info;
+  }
+
+  public void GET_NAME_INFO(String data_info) {
+    this.data_info = data_info;
+  }
 
   public void JSON_IT(String data_to_transfer) {
     try {
       File to_json = new File("//home//runner//info.json");
       Formatter convert = new Formatter(to_json);
-      convert.format("%s %s", "{\n\"type\":",this.quote+data_to_transfer+this.quote+"\n}");
+      convert.format("%s %s%s %s", "{\n\"type\":",this.quote+data_to_transfer+this.quote+",\n",this.quote+data_to_transfer+"_info"+this.quote+":",this.quote+this.data_info+this.quote+"\n}");
       convert.close();
     } catch(Exception e) {
       System.out.println(e);
